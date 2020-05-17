ee1161922646c971e0012db3328304a21e0be3c3
End Point <ahmedmohamed23345@gmail.com>
2020-05-17 11:36:41 +0200

    ch

--- app/src/main/java/com/creativeshare/zapyhakoom/Tags/Tags.java
+++ app/src/main/java/com/creativeshare/zapyhakoom/Tags/Tags.java
@@ -3,7 +3,7 @@ package com.creativeshare.zapyhakoom.Tags;
 public class Tags {
     public static String pref_lang = "ar";
     public static String base_url = "http://zbayeh.com";
-    public static String base_IMage_url = "http://marasi.sa/public/uploads";
+    public static String base_IMage_url = "http://zbayeh.com/public/uploads";
 
     public static final String session_login = "login";
     public static final String session_logout = "logout";
